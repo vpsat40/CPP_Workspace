6d4b9d2488c3f7b95865cf6b37c6e0b28fb91203
vpsat40 <sat.subscribes@gmail.com>
2021-05-11 19:51:01 +1000

    Create Template_Intro.h

---
+++ STL_Exercises/Template_Intro.h
@@ -0,0 +1,53 @@
+/*
+ * ClassTemplate.cpp
+ *
+ *  Created on: Apr 20, 2021
+ *      Author: satish.vodapally
+ */
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T funcSquare(T x)
+{
+	return x*x;
+}
+
+template <typename CT>
+class BoVector
+{
+	CT mArr[1000];
+	int size;
+
+public:
+	BoVector(): size(0) {};
+
+	void push(CT x)
+	{
+		mArr[size] = x;
+		size++;
+	}
+	void print()
+	{
+		for(int i=0; i<size; i++)
+			cout << mArr[i] << endl;
+	}
+
+	int getSize() { return size; }
+
+	CT getElement(int i) { return mArr[i]; }
+
+};
+
+
+template <typename MT>
+BoVector<MT> operator*(BoVector<MT>& rhs1, BoVector<MT>& rhs2)
+{
+	BoVector<MT> ret;
+	for(int i=0; i < rhs1.getSize(); i++)
+		ret.push(rhs1.getElement(i)*rhs2.getElement(i));
+
+	return ret;
+}
+
