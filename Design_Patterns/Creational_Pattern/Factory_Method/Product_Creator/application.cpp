86281236fcc8087fd474aaba28aee0b8873cd272
satish.vodapally <satish.vodapally@seeingmachines.com>
2023-07-13 12:31:03 +1000

    Product_Creator example
    Example Product_Creator code
    
    See: https://refactoring.guru/design-patterns/factory-method/cpp/example

---
+++ Design_Patterns/Creational_Pattern/Factory_Method/Product_Creator/application.cpp
@@ -0,0 +1,34 @@
+#include "creator.h"
+
+/**
+ * The client code works with an instance of a concrete creator, albeit through
+ * its base interface. As long as the client keeps working with the creator via
+ * the base interface, you can pass it any creator's subclass.
+ */
+void ClientCode(const Creator& creator) 
+{
+  // ...
+  std::cout << "Client: I'm not aware of the creator's class, but it still works.\n"
+            << creator.SomeOperation() << std::endl;
+  // ...
+}
+
+/**
+ * The Application picks a creator's type depending on the configuration or
+ * environment.
+ */
+
+int main() 
+{
+  std::cout << "App: Launched with the ConcreteCreator1.\n";
+  Creator* creator = new ConcreteCreator1();
+  ClientCode(*creator);
+  std::cout << std::endl;
+  std::cout << "App: Launched with the ConcreteCreator2.\n\n";
+  Creator* creator2 = new ConcreteCreator2();
+  ClientCode(*creator2);
+
+  delete creator;
+  delete creator2;
+  return 0;
+}
